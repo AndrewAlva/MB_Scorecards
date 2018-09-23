ce18434bafabf44ba494400970b3cc8004b5b9d9
Mandelbrot Studio <mandelbrot@iMac-de-Mandelbrot-2.local>
2018-09-23 14:20:27 -0500

    First try: PHP and JS functionality test.

---
+++ app/assets/send_data.php
@@ -0,0 +1,42 @@
+<?php
+	$recipient = "andrew@mandelbrot.mx";
+	$subject = "Request of TGP Franchise Opportunity";
+
+	$name = $_POST['name'];
+	$lastname = $_POST['lastname'];
+	$tel = $_POST['tel'];
+	$email = $_POST['email'];
+	$feedback = $_POST['feedback'];
+
+	$formcontent="Hey Ryan, this is a new email written by a user from modrnbusiness.com/TGP; this is what they say:\n\nHi, my name is $name $lastname and my email address is: $email\n\nI have a message for you, $feedback";
+	$mailheader = "From: $email \r\n";
+
+	// Write in the .txt file
+		$new_subscriber = "Full name: $name $lastname, Cellphone: $tel, Email: $email"."\n";
+		$file = fopen("mbo_repo_requesters.txt", "r+") or die("Unable to open file!");
+		fread($file,filesize("mbo_repo_requesters.txt"));
+		fwrite($file, $new_subscriber);
+		fclose($file);
+		echo("Subscriber data collected correctly.");
+
+	// Send email with user data
+		$mail_sent = mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+		if ($mail_sent == true){ ?>
+			<script language="javascript" type="text/javascript">
+				// alert('Great! Your message was sent successfully.');
+				// window.location = '/';
+				console.log('Great! Your message was sent successfully.');
+			</script>
+
+		<?php 
+		} else { 
+		?>
+			<script type="text/javascript">
+				alert('There was an error and the message could not be sent, try again and if the error persists, try to reach us on our social media channels.');
+				window.location = '/';
+			</script>
+		
+		<?php 
+		} 
+	}	
+?>
--- app/js/main.js
+++ app/js/main.js
@@ -14,19 +14,15 @@
 			alert("You must fill all the fields marked with a red asterisk to complete the request!");
 		} else {
 			$.ajax({
-				url: "/request_data.php",
+				url: "/send_data.php",
 				type: "POST",
-				data: { name: $('#contact-name').val(), lastname: $('#contact-lastname').val(), tel: $("#contact-cellphone").val(), email: $('#contact-email').val() }
+				data: { name: $('#contact-name').val(), lastname: $('#contact-lastname').val(), tel: $("#contact-cellphone").val(), email: $('#contact-email').val(), feedback: $('#contact-feedback').val() }
 			})
 			.done(function(e) {
 				console.log("e",e);
 				$('#request-wrapper').addClass('fade-out');
 				$('.answer-wrapper').html('<h2>Thanks for joining the community!</h2><p>You will receive an email with the complete report of ©The Gents Place very soon, meanwhile you can visit Modrn Businss complete site to know more about our purpose and get more content.</p><a href="https://modrnbusiness.com/" class="mb-button">Visit Full Site</a>');
 				$('.answer-wrapper').addClass('fade-in');
-				// TweenMax.to($('.subscribe-section'), 0.6, {autoAlpha: 0, display: 'none', onComplete:function(){
-				// 	$('.subscribe-section').html('<h3 class="intro-title h2"> Thanks for subscribing! </h3> <p class="intro-description h4"> Wait news from us soon. </p>').fadeIn();
-				// 	TweenMax.to($('.subscribe-section'), 0.6, {autoAlpha: 1, display: 'block'  });
-				// } });
 			});
 		}
 	});
