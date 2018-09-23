32af077cff0394eb40a8eb9841474a1992cc67b3
Mandelbrot Studio <mandelbrot@iMac-de-Mandelbrot-2.local>
2018-09-23 15:40:11 -0500

    PHP and JS functionality ready and tested. Syntax of mail improved.

--- app/assets/send_data.php
+++ app/assets/send_data.php
@@ -8,35 +8,34 @@
 	$email = $_POST['email'];
 	$feedback = $_POST['feedback'];
 
-	$formcontent="Hey Ryan, this is a new email written by a user from modrnbusiness.com/TGP; this is what they say:\n\nHi, my name is $name $lastname and my email address is: $email\n\nI have a message for you, $feedback";
+	$formcontent="Hey Ryan, this is a new email written by a user from modrnbusiness.com/TGP; this is what they say:\n\nHi, my name is $name $lastname, my email address is: $email and my cellphone number is: $tel.\n\nI have a message for you\n $feedback";
 	$mailheader = "From: $email \r\n";
 
 	// Write in the .txt file
-		$new_subscriber = "Full name: $name $lastname, Cellphone: $tel, Email: $email"."\n";
-		$file = fopen("mbo_repo_requesters.txt", "r+") or die("Unable to open file!");
-		fread($file,filesize("mbo_repo_requesters.txt"));
-		fwrite($file, $new_subscriber);
-		fclose($file);
-		echo("Subscriber data collected correctly.");
+	$new_subscriber = "Full name: $name $lastname, Cellphone: $tel, Email: $email"."\n";
+	$file = fopen("mbo_repo_requesters.txt", "r+") or die("Unable to open file!");
+	fread($file,filesize("mbo_repo_requesters.txt"));
+	fwrite($file, $new_subscriber);
+	fclose($file);
+	echo("Subscriber data collected correctly.");
 
 	// Send email with user data
-		$mail_sent = mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
-		if ($mail_sent == true){ ?>
-			<script language="javascript" type="text/javascript">
-				// alert('Great! Your message was sent successfully.');
-				// window.location = '/';
-				console.log('Great! Your message was sent successfully.');
-			</script>
+	$mail_sent = mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+	if ($mail_sent == true){ ?>
+		<script language="javascript" type="text/javascript">
+			// alert('Great! Your message was sent successfully.');
+			// window.location = '/';
+			console.log('Great! Your message was sent successfully.');
+		</script>
 
-		<?php 
-		} else { 
-		?>
-			<script type="text/javascript">
-				alert('There was an error and the message could not be sent, try again and if the error persists, try to reach us on our social media channels.');
-				window.location = '/';
-			</script>
-		
-		<?php 
-		} 
-	}	
+	<?php 
+	} else {
+	?>
+		<script type="text/javascript">
+			alert('There was an error and the message could not be sent, try again and if the error persists, try to reach us on our social media channels.');
+			window.location = '/';
+		</script>
+	
+	<?php 
+	}
 ?>
--- app/js/main.js
+++ app/js/main.js
@@ -19,7 +19,7 @@
 				data: { name: $('#contact-name').val(), lastname: $('#contact-lastname').val(), tel: $("#contact-cellphone").val(), email: $('#contact-email').val(), feedback: $('#contact-feedback').val() }
 			})
 			.done(function(e) {
-				console.log("e",e);
+				// console.log("e",e);
 				$('#request-wrapper').addClass('fade-out');
 				$('.answer-wrapper').html('<h2>Thanks for joining the community!</h2><p>You will receive an email with the complete report of ©The Gents Place very soon, meanwhile you can visit Modrn Businss complete site to know more about our purpose and get more content.</p><a href="https://modrnbusiness.com/" class="mb-button">Visit Full Site</a>');
 				$('.answer-wrapper').addClass('fade-in');
